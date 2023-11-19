0d6d8a62ed1f6d975d6ef883bcccc46c0959144e
aerotecnia99 <valentinacamph@uc.cl>
2023-11-19 19:19:58 -0300

    style(tailwind): add custom colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        base: ['"Roboto"', 'sans-serif'],
+      },
       colors: {
-  
+        primary: {
+          light: '#27A5EE',
+          DEFAULT: '#3818D9',
+          dark: '#291B74',
+        },
+        secondary: {
+          DEFAULT: '#FF5C81',
+        },
+        backgroundGrey: '#F8F8FA',
+        grey: '#687C94',
+        
       },
     },
   },
