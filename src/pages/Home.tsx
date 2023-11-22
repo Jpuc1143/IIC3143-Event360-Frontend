db1543d40a82881e586dc3d24542c8ad9dda2ff8
aerotecnia99 <valentinacamph@uc.cl>
2023-11-22 19:11:29 -0300

    feat(landing-page): Home, searchBar and Footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,8 @@
 import EventCard from "../components/EventCard";
-import main_img from "../assets/auditorio-luksic.jpg";
+import SearchBar from "../components/SearchBar";
+import main_img from "../assets/party.jpeg";
+import { useEffect, useState } from "react";
+import { getRequest } from "../api/queries";
 
 const evento = {
   name: "DCCTarreo",
@@ -12,17 +15,31 @@ const evento = {
 } as Event;
 
 export default function Home() {
+  useEffect(() => {
+    const getEvents = async () => {
+      const { data } = await getRequest('/events', 'token');
+      if (data) setEvents(data);
+    }
+    getEvents();
+  }, []);
+
+  const [events, setEvents] = useState<Event[]>([]);
+
+
   return (
     <div className="mx-16 my-8">
-      <img className="w-full h-96 rounded-xl" src={main_img} alt="main-img"/>
-      <div className="bg-primary-dark h-36 w-full rounded-xl mx-16">
-        hola
+      <div className="flex flex-col items-center relative mb-16">
+        <h1 className="py-4 absolute mt-12 bg-white/40 w-full flex justify-center">
+          <span className="text-primary-dark text-5xl font-bold">Event</span>
+          <span className="text-primary-light text-5xl font-bold">360</span>
+        </h1>
+        <img className="w-full max-h-96 rounded-xl object-cover" src={main_img} alt="main-img"/>
+        <SearchBar /> 
       </div>
       <div className="mx-32">
         <h1 className="text-4xl font-bold my-8 text-primary">Eventos</h1>
         <div className="grid grid-cols-3 gap-8">
-          <EventCard event={evento} />
-          <EventCard event={evento} />
+          {events.map((event) => <EventCard event={event} />)}
           <EventCard event={evento} />
           <EventCard event={evento} />
           <EventCard event={evento} />
