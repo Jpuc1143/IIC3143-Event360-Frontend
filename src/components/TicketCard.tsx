0448ffeca7b82d048300450eef5e10dd995d65ec
Martín Pérez <mprez@uc.cl>
2023-11-22 23:26:08 -0300

    🚀 fixed lint issues

--- src/components/TicketCard.tsx
+++ src/components/TicketCard.tsx
@@ -2,11 +2,11 @@ import ticket_img from '../assets/ticket.png';
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function TicketCard({ ticket }: { ticket: TicketType }) {
-    const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+    const { isAuthenticated } = useAuth0();
     /* alt  ticket.name ???? */
     return (
         <div className="max-w-sm rounded-xl overflow-hidden shadow-lg hover:shadow-xl">
-            <img className="w-full" src={ticket_img} />
+            <img alt="" className="w-full" src={ticket_img} />
             <div className='px-6 py-4'>
                 <div className='mb-2'>
                     <h3 className='font-bold text-xl text-primary-dark'>
@@ -18,7 +18,7 @@ export default function TicketCard({ ticket }: { ticket: TicketType }) {
                     <div className='text-base font-bold'>
                         Restantes: {ticket.amount}
                     </div>
-                    {ticket.domainWhiteList != 'all' && (
+                    {ticket.domainWhiteList !== 'all' && (
                         <div className='text-base font-bold'>
                             (Solo válido para correos {ticket.domainWhiteList})
                         </div>
