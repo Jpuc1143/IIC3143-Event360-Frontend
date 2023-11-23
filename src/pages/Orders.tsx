ef2530b1e2277d09d951c0a23d876fcf00e1303d
Juan Pablo Lozano <jp.lozano@uc.cl>
2023-11-23 02:06:20 -0300

    Orden request

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Order from "../components/OrderCard";
+import { useEffect, useState } from "react";
+import { getRequest } from "../api/queries";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const order1 = {
   id: 1,
@@ -19,15 +22,30 @@ const order3 = {
   price: 1,
 };
 
-export default function Orders() {
+export default function Home() {
+  useEffect(() => {
+    const getOrders = async () => {
+      const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+      const { data } = await getRequest('/events/' + user.id, 'token');
+      if (data) setOrders(data);
+    }
+    getOrders();
+  }, []);
+
+  const [orders, setOrders] = useState<Order[]>([]);
+
+
   return (
     <div className="mx-16 my-8">
-      <h1 className="text-4xl font-bold my-8 text-primary">Órdenes</h1>
-      <div className="flex flex-wrap justify-between">
-        <Order order={order1} />
-        <Order order={order2} />
-        <Order order={order3} />
+      <div className="mx-32">
+        <h1 className="text-4xl font-bold my-8 text-primary">Órdenes</h1>
+        <div className="grid grid-cols-3 gap-8">
+          {orders.map((order) => <Order order={order} />)}
+          <Order order={order1} />
+          <Order order={order2} />
+          <Order order={order3} />
+        </div>
       </div>
     </div>
-  );
+  )
 }
