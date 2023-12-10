648fd6f061455a2dcd0cb4c8f3d6e9683c118218
Jpuc1143 <jaime.perez@uc.cl>
2023-12-10 16:35:33 -0300

    Feat: verificar tickets usando qr

--- src/pages/EditEvent.tsx
+++ src/pages/EditEvent.tsx
@@ -1,7 +1,11 @@
+import { Link, useParams } from "react-router-dom";
+
 export default function EditEvent() {
+  const eventId = useParams<{ id: string }>().id;
   return (
     <div>
       <h1>EditEvent</h1>
+      <Link to={`/verify-ticket/${eventId}`}>Verificar tickets</Link>
     </div>
   );
 }
