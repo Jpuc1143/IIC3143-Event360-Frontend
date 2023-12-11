b0deda5ea19d298037e78e16ecb70e915f137789
martin-santiago <mprez@uc.cl>
2023-12-10 21:32:12 -0300

    🚀 addded style to pagination

--- src/pages/event/Events.tsx
+++ src/pages/event/Events.tsx
@@ -100,16 +100,26 @@ export default function Events() {
           <EventCard event={event} />
         ))}
       </div>
-      <nav className="mx-auto mt-24">
-        <ul className="flex gap-6">
-          <li className="page-item">
-            <button className="page-link" onClick={prePage}>
+      <nav className="mt-24 flex justify-center ">
+        <ul className="inline-flex -space-x-px text-base h-10">
+          <li className="">
+            <button
+              className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
+              onClick={prePage}
+            >
               Anterior
             </button>
           </li>
-          <li>{currentPage}</li>
-          <li className="page-item">
-            <button className="page-link" onClick={nextPage}>
+          <li>
+            <div className="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700">
+              {currentPage}
+            </div>
+          </li>
+          <li className="">
+            <button
+              className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
+              onClick={nextPage}
+            >
               Siguiente
             </button>
           </li>
